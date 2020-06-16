dd2a8aa5ce358d6ddb14b08cd267e6f8ba8e5e27
kris <kristmitina@gmail.com>
2020-06-16 20:23:37 +0300

    release v1

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by DVKSH on 28.05.2020.
+//  Copyright © 2020 Kris Mitina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "I am Rich!"
+        
+    }
+
+
+}
+
